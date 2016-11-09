4d0484d23287d6956c1edc704f40a1e83f3fb814
John Kyungin Yoo <m3c9@bowen.ugrad.cs.ubc.ca>
2016-11-09 00:52:41 -0800

    updated action_page.php for more functionality. needs improvement though

--- justcuz-test/test/action_page.php
+++ justcuz-test/test/action_page.php
@@ -1,7 +1,7 @@
 <html>
 <?php
 if ($c=OCILogon("ora_m3c9", "a39296132", "dbhost.ugrad.cs.ubc.ca:1522/ug")) { 
-  echo "Successfully connected to Oracle.\n"; 
+  //echo "Successfully connected to Oracle.\n"; 
 } else { 
   $err = OCIError(); 
   echo "Oracle Connect Error " . $err['message']; 
@@ -9,6 +9,32 @@ if ($c=OCILogon("ora_m3c9", "a39296132", "dbhost.ugrad.cs.ubc.ca:1522/ug")) {
 //create 4 digit cid at random
 //get cids from table
 //if
+$inum = $_GET["itemnum"];
+
+$sti = oci_parse($c, "SELECT count from inventory_tracks where item_num ='". $inum. "'");
+if (!$sti) {
+    $e = OCIError($c);
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+$r = oci_execute($sti);
+if (!$r) {
+    $e = OCIERROR($sti);
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+$count;
+while (($row = oci_fetch_array($sti, OCI_NUM)) != false) {
+    $count=$row[0];
+}
+echo $count;
+if($count <=0){
+	echo "Sorry, sold out!";
+	header( "refresh:3;url=http://www.ugrad.cs.ubc.ca/~m3c9/temp/justcuz/justcuz-test/test/main.html" );
+}
+else{
+$sql1 = "UPDATE inventory_tracks set count = count - 1 where item_num ='". $inum. "'";
+$std=oci_parse($c, $sql1);
+oci_execute($std);
+
 $stid = oci_parse($c, 'select cid from customer');
 if (!$stid) {
     $e = OCIError($c);
@@ -19,7 +45,8 @@ if (!$r) {
     $e = OCIERROR($stid);
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-$cid = 4208;
+
+$cid = rand(1000,9999);
 echo $cid;
 $exists = True;
 while($exists){
@@ -27,12 +54,9 @@ while($exists){
     foreach ($row as $item) {
         if($item == $cid){
         	$cid = rand(1000, 9999);
-			echo $cid;
-			echo "Fail! CID doesn't already exist in table";
         	$exists=True;
         }
         else{
-        	echo "Success! CID doesn't already exist in table";
         	$exists=False;
         }
     }
@@ -46,6 +70,6 @@ $cardtype = $_GET["cardtype"];
 
 $sql = "INSERT INTO customer(cid,name,email,address,card_num,card_type) VALUES('$cid', '$cname', '$email', '$address', '$cardnum', '$cardtype')";
 $st=oci_parse($c, $sql);
-oci_execute($st);
+oci_execute($st);}
 ?>
 </html>
