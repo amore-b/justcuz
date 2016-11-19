8553c48174c83fdaca90816a6f0efc4b67929023
John Kyungin Yoo <m3c9@bowen.ugrad.cs.ubc.ca>
2016-11-18 18:45:15 -0800

    sends customer email with order info

--- justcuz-test/test/action_page.php
+++ justcuz-test/test/action_page.php
@@ -1,12 +1,12 @@
 <html>
 <?php
 if ($c=OCILogon("ora_m3c9", "a39296132", "dbhost.ugrad.cs.ubc.ca:1522/ug")) { 
-  echo "Successfully connected to Oracle.\n"; 
+  echo ".\n"; 
 } else { 
   $err = OCIError(); 
   echo "Oracle Connect Error " . $err['message']; 
+  die;
 }
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!NEGATIVE POINTS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 //create 4 digit cid at random
 //get cids from table
@@ -28,11 +28,13 @@ $sti = oci_parse($c, "SELECT count from inventory_tracks where item_num ='". $in
 if (!$sti) {
     $e = OCIError($c);
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    die;
 }
 $r = oci_execute($sti);
 if (!$r) {
     $e = OCIERROR($sti);
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    die;
 }
 while ($row = oci_fetch_array($sti, OCI_ASSOC+OCI_RETURN_NULLS)) {
     foreach ($row as $item) {
@@ -43,8 +45,9 @@ while ($row = oci_fetch_array($sti, OCI_ASSOC+OCI_RETURN_NULLS)) {
 oci_free_statement($sti);
 //if the quantity desired is greater than the count, redirect them to the homepage
 if($count - $quantity < 0){ //TODO: this is executing before the update table. figure it out
-    echo "Sorry, insufficient quantity!";
-    //header( "refresh:3;url=http://www.ugrad.cs.ubc.ca/~m3c9/temp/justcuz/justcuz-test/test/main.html" );
+    echo "Sorry, insufficient quantity! Please reselect a lower quantity. We promise to restock a$ap. Redirecting...";
+    header( "refresh:5;url=http://www.ugrad.cs.ubc.ca/~m3c9/temp/justcuz/justcuz-test/test/main.html" );
+    die;
 }
 
 //otherwise, add the customer to the db, deduct the existing quantity of the item, and track the order
@@ -84,11 +87,13 @@ $stid = oci_parse($c, 'select cid from customer');
 if (!$stid) {
     $e = OCIError($c);
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    die;
 }
 $r = oci_execute($stid);
 if (!$r) {
     $e = OCIERROR($stid);
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    die;
 }
 $cid = rand(1000,9999);
 $exists = True;
@@ -106,9 +111,21 @@ while($exists){
     }
 }
 oci_free_statement($stid);
-$sql = "INSERT INTO customer(cid,name,email,address,card_num,card_type) VALUES('$cid', '$cname', '$email', '$address', '$cardnum', '$cardtype')";
+
+$sql69 = "INSERT INTO users VALUES('$email', 2)";
+$st69=oci_parse($c, $sql69);
+if(!oci_execute($st69)){
+    //echo "Failed to add user! Redirecting...";
+}
+oci_free_statement($st69);
+
+$sql = "INSERT INTO customer VALUES('$cid', '$cname', '$email', '$address', '$cardnum', '$cardtype')";
 $st=oci_parse($c, $sql);
-oci_execute($st);
+if(!oci_execute($st)){
+    echo "Failed to add customer! Redirecting...";
+    header( "refresh:3;url=http://www.ugrad.cs.ubc.ca/~m3c9/temp/justcuz/justcuz-test/test/main.html" );
+    die;
+}
 oci_free_statement($st);
 }
 else{
@@ -156,25 +173,28 @@ $std=oci_parse($c, $sql1);
 oci_execute($std);
 oci_free_statement($st);
 //insert new order in order_delivers_buys table NOT FUNCTIONAL
-echo "order number: " . $ordernum;
-echo " item number: " . $inum;
-echo " employee id: " . $eid;
-echo " cust id: " . $cid;
-echo " quantity: " . $quantity;
-
 $sql2 = "INSERT INTO order_delivers_buys VALUES ('$ordernum', '$totalprice', '$inum', '$eid', '$cid', sysdate, '$quantity')";
 $std1=oci_parse($c, $sql2);
 if (!$std1) {
-    echo "order delivers buys insert query parse failed";
+    echo "order delivers buys insert query parse failed. Redirecting...";
+    header( "refresh:3;url=http://www.ugrad.cs.ubc.ca/~m3c9/temp/justcuz/justcuz-test/test/main.html" );
+    die;
 }
 $r2 = oci_execute($std1);
 if (!$r2) {
     //$e = oci_error();   // For oci_connect errors pass no handle
-    echo "order delivers buys insert query execution failed";
+    echo "order delivers buys insert query execution failed. Redirecting...";
+    header( "refresh:3;url=http://www.ugrad.cs.ubc.ca/~m3c9/temp/justcuz/justcuz-test/test/main.html" );
+    die;
 }
 oci_free_statement($std1);
 
 oci_close($c);
+echo "Purchase successful! Sending confirmation email. Redirecting... ";
+$discount = $totalprice - $points;
+$message = "Hi " . $cname . ", here's your tl;dr order information:\r\n" . "Order number: " . $ordernum . "\r\n" . "Total paid: $" . $discount ."\r\n" . "Thank you for shopping at Stuff 'n Things :)";
+mail($email, 'Order receipt', $message);
+header( "refresh:5;url=http://www.ugrad.cs.ubc.ca/~m3c9/temp/justcuz/justcuz-test/test/main.html" );
 }
 ?>
 </html>
